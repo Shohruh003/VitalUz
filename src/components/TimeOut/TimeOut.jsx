026d12d03dcc977d26f5758dded10a6290b6a025
Shohruh003 <shohruhazimov003@gmail.com>
2023-07-28 23:41:08 +0500

    Add TimeOut localStorage

--- src/components/TimeOut/TimeOut.jsx
+++ src/components/TimeOut/TimeOut.jsx
@@ -2,76 +2,93 @@ import React, { useState, useEffect } from 'react';
 import './timeOut.css'
 
 function TimeOut({isDivVisible, setIsDivVisible}) {
+  const [isDiv, setIsDiv] = useState(false)
 
-const [countdown, setCountdown] = useState(parseInt(localStorage.getItem('countdown')) || 36 * 60 * 60);
-const [isRunning, setIsRunning] = useState(true);
-const [isDiv, setIsDiv] = useState(false);
-const [hours, setHours] = useState(parseInt(localStorage.getItem('hours')) || 36);
-const [minutes, setMinutes] = useState(parseInt(localStorage.getItem('minutes')) || 0);
-const [seconds, setSeconds] = useState(parseInt(localStorage.getItem('seconds')) || 0);
+  const [countdown, setCountdown] = useState(36 * 60 * 60 * 1000);
+  const [deliveryTime, setDeliveryTime] = useState(
+    localStorage.getItem("count") || 0
+  );
+  const [currentTime, setCurrentTime] = useState(0);
 
-useEffect(() => {
-const intervalId = setInterval(() => {
-if (isRunning && countdown > 0) {
-setCountdown(prevCountdown => {
-const newCountdown = prevCountdown - 1;
-localStorage.setItem('countdown', newCountdown);
-const newHours = Math.floor(newCountdown / 3600);
-const newMinutes = Math.floor((newCountdown % 3600) / 60);
-const newSeconds = newCountdown % 60;
-setHours(newHours);
-setMinutes(newMinutes);
-setSeconds(newSeconds);
-localStorage.setItem('hours', newHours);
-localStorage.setItem('minutes', newMinutes);
-localStorage.setItem('seconds', newSeconds);
-return newCountdown;
-});
-}
-}, 1000);
-return () => clearInterval(intervalId);
-}, [countdown, isRunning]);
+  const [extraTime, setExtraTime] = useState(0);
 
-const handleButtonClick = () => {
-setIsRunning(false);
-setIsDivVisible(false);
-setIsDiv(true)
-localStorage.setItem('isDivVisible', JSON.stringify(false));
-};
+  const handleStart = () => {
+    localStorage.setItem("count", new Date().getTime() + countdown);
+    setDeliveryTime(new Date().getTime() + countdown);
+  };
 
-const times = 36 * 60 * 60
-const tem = times - countdown
+  const handleStop = () => {
+    setExtraTime(countdown - currentTime);
+    setCurrentTime(0);
+  };
 
-const hour = Math.floor(tem / 3600);
-const minute = Math.floor((tem % 3600) / 60);
-const second = tem % 60;
+  useEffect(() => {
+    setCurrentTime(+deliveryTime - new Date().getTime());
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentTime > 0) {
+        setCurrentTime(+deliveryTime - new Date().getTime());
+      }
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [currentTime, deliveryTime]);
+
+  function secondsToHms(seconds) {
+    let d = Number(seconds / 1000);
+
+    if (d <= 0) {
+      return "00:00:00";
+    } else {
+      let h = Math.floor(d / 3600);
+      let m = Math.floor((d % 3600) / 60);
+      let s = Math.floor((d % 3600) % 60);
+
+      let hDisplay = h <= 9 ? "0" + h + ":" : h + ":";
+      let mDisplay = m <= 9 ? "0" + m + ":" : m + ":";
+      let sDisplay = s <= 9 ? "0" + s : s;
+
+      return hDisplay + mDisplay + sDisplay;
+    }
+  }
+
+
+  useEffect(() => {
+    const storedIsDivVisible = localStorage.getItem('isDivVisible');
+    if (storedIsDivVisible) {
+      setIsDivVisible(JSON.parse(storedIsDivVisible));
+    }
+  }, [setIsDivVisible]);
+
+  const handleButtonClick = () => {
+    handleStop()
+    setIsDivVisible(false);
+    setIsDiv(true);
+    localStorage.setItem('isDivVisible', JSON.stringify(false));
+  };
+
 const handleButton = () => {
-setIsDiv(false)
-localStorage.removeItem('countdown');
-localStorage.removeItem('hours');
-localStorage.removeItem('minutes');
-localStorage.removeItem('seconds');
+  handleStart()
+  setIsDiv(false);
 window.location.reload();
-};
 
-useEffect(() => {
-const storedIsDivVisible = localStorage.getItem('isDivVisible');
-if (storedIsDivVisible) {
-setIsDivVisible(JSON.parse(storedIsDivVisible));
-}
-}, [setIsDivVisible]);
+};
 
 return (
 <div>
 {isDivVisible && (
 <div className='timeOut'>
 <p>Yetkazib berish vaqti</p>
-<p>{hours} : {minutes} : {seconds}</p>
+<p>{secondsToHms(+currentTime)}</p>
 <button onClick={handleButtonClick}>Qabul qildim!</button>
 </div>
 )}
 
 {isDiv && (
 
-<div className='timeOut'> <p>Yetkazib berilgan vaqti</p> <p>{hour} : {minute}: {second}</p> <button className='timeOut-btn' onClick={handleButton}>Ok</button> </div> )} </div> ); }
+<div className='timeOut'> <p>Yetkazib berilgan vaqti</p>
+<p> {secondsToHms(extraTime)}</p>
+<button className='timeOut-btn' onClick={handleButton}>Ok</button> </div> )} </div> ); }
+
 export default TimeOut;
